b52cae36ede31bded599d42d2d7a7266f3aa0a9b
ismailgraphix <ismailgrapix@gmail.com>
2024-08-09 18:31:34 +0100

    course chapter and layout completion

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -12,7 +12,7 @@ export const NavbarRoutes = () => {
     
     
     const isTeacherPage = pathname?.startsWith("/teacher")
-    const isPlayerPage = pathname?.includes("/chapter")
+    const isPlayerPage = pathname?.includes("/courses")
     const isSearchPage = pathname === "/search";
 
     return (
